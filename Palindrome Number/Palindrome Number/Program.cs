6074f57686c00e106973bd702463cada1ebb09f8
Furkan Özkan <furkan.105398@gmail.com>
2023-02-13 14:42:18 +0300

    Palindrome Number
    A palindromic number (also known as a numeral palindrome or a numeric palindrome) is a number (such as 16461) that remains the same when its digits are reversed. In other words, it has reflectional symmetry across a vertical axis.

---
+++ Palindrome Number/Palindrome Number/Program.cs
@@ -0,0 +1,34 @@
+﻿//Given an integer x, return true if x is a palindrome integer.
+
+//An integer is a palindrome when it reads the same backward as forward.
+
+//Input: x = 121
+//Output: true
+//Explanation: 121 reads as 121 from left to right and from right to left.
+
+
+//Input: x = 10
+//Output: false
+//Explanation: Reads 01 from right to left.Therefore it is not a palindrome.
+
+
+
+bool isPalindrome(int num)
+{
+    int tempNum = num;
+    int reverseOfNum = 0;
+
+    if (tempNum < 0)
+    {
+        return false;
+    }
+    while(tempNum > 0)
+    {
+        reverseOfNum = reverseOfNum * 10 + tempNum % 10;
+        tempNum /= 10;
+    }
+    return num == reverseOfNum;
+}
+
+Console.WriteLine(isPalindrome(10));
+
