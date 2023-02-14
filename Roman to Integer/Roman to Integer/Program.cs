212844ea5c73ce8b91e53a10be32568896444370
Furkan Özkan <furkan.105398@gmail.com>
2023-02-14 17:59:02 +0300

    Roman To Intefer
    A program that returns integer value to numbers written with roman numerals

---
+++ Roman to Integer/Roman to Integer/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+ static int RomanToInteger(string roman)
+{
+    Dictionary<char, int> RomanMap = new Dictionary<char, int>()
+    {
+        {'I', 1},
+        {'V', 5},
+        {'X', 10},
+        {'L', 50},
+        {'C', 100},
+        {'D', 500},
+        {'M', 1000}
+    };
+
+    int number = 0;
+    for (int i = 0; i < roman.Length; i++)
+    {
+        if (i + 1 < roman.Length && RomanMap[roman[i]] < RomanMap[roman[i + 1]])
+        {
+            number -= RomanMap[roman[i]];
+        }
+        else
+        {
+            number += RomanMap[roman[i]];
+        }
+    }
+    return number;
+}
+
+Console.WriteLine(RomanToInteger(""));
