1d7a8a8c05faa8af26edc9b88f39d26e53e57385
Furkan Özkan <furkan.105398@gmail.com>
2023-02-21 20:21:05 +0300

    Longest Common Prefix Solution
    Write a function to find the longest common prefix string amongst an array of strings.
    
    If there is no common prefix, return an empty string "".

---
+++ Longest Common Prefix Solution/Longest Common Prefix Solution/Program.cs
@@ -0,0 +1,24 @@
+﻿string[] input = {"Bilmek", "bilmem","bilhakis"};
+
+Console.WriteLine("The longest Common"+ " Prefix is : "+ longestCommonPrefix(input));
+
+string longestCommonPrefix(string[] input)
+{
+    int size = input.Length;
+    if (size == 0)
+        return "";
+    if (size == 1)
+        return input[0];
+
+    Array.Sort(input);
+
+    int end = Math.Min(input[0].Length, input[size - 1].Length);
+
+    int i = 0;
+    while (i < end && input[0][i] == input[size - 1][i])
+        i++;
+
+        string pre = input[0].Substring(0, i);
+        return pre;
+    
+}
