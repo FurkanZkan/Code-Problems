1f4dd3017695ddb6c6ac87b11ff27a0ae6111003
Furkan Özkan <furkan.105398@gmail.com>
2023-02-28 21:32:45 +0300

    Happy Number
    A number is called happy if it leads to 1 after a sequence of steps wherein each step number is replaced by the sum of squares of its digit that is if we start with Happy Number and keep replacing it with digits square sum, we reach 1.

---
+++ Happy Number/Happy Number/Program.cs
@@ -0,0 +1,26 @@
+﻿int n, r = 1, num, sum = 0;
+
+Console.Write("Enter Number : ");
+n = Convert.ToInt32(Console.ReadLine());
+
+num = n;
+while (num > 9)
+{
+    while (num > 0)
+    {
+        r = num % 10;
+        sum = sum + (r * r);
+        num = num / 10;
+    }
+    num = sum;
+    sum = 0;
+}
+if (num == 1)
+{
+    Console.WriteLine("Happy Number");
+}
+else
+{
+    Console.WriteLine("Not Happy Number");
+}
+  
