ddd11e1833c0f13cac4edee1296602e3dd3a47a1
Furkan Özkan <furkan.105398@gmail.com>
2023-03-01 21:38:15 +0300

    Fibonacci Series

---
+++ Fibonacci Series/Fibonacci Series/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+
+int fibonacciSeries(int length)
+{
+    if (length == 0 || length == 1)
+        return 1;
+
+
+    return fibonacciSeries(length - 1) + fibonacciSeries(length -2);
+}
+
+
+Console.Write("Length of the Fibonacci Series: ");
+int length = Convert.ToInt32(Console.ReadLine());
+for (int i = 0; i < length; i++)
+{
+    Console.Write("{0} ", fibonacciSeries(i));
+}
+
+
+
