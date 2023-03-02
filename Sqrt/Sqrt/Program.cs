f442103663cec383d69f75431e03bb008b707856
Furkan Özkan <furkan.105398@gmail.com>
2023-03-02 23:29:04 +0300

    Sqrt
    program to find the square root of any number

---
+++ Sqrt/Sqrt/Program.cs
@@ -0,0 +1,32 @@
+﻿double number;
+start:
+Console.Write("What number do you want to find the square root of :");
+number = double.Parse(Console.ReadLine());
+if (number < 0)
+{
+    Console.WriteLine("please enter a value greater than 0 ");
+    goto start;
+}
+else
+{
+    Console.WriteLine("{0} square root of the number : {1}", number, NewtonSqrtSlgorithm(number));
+}
+
+double NewtonSqrtSlgorithm(double num)
+{
+    double x1 = 0;
+    double x2 = num / 2;
+    if (num != 0)
+    {
+        do
+        {
+            x1 = x2;
+            x2 = (x1 + (num / x1)) / 2;
+        } while (x2 - x1 != 0);
+    }
+    else
+    {
+        x2 = 0;
+    }
+    return x2;
+}
